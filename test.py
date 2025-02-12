be587a28999e932fb174f7d5797e936d065f57a5
Stef van Herk <547380@student.fontys.nl>
2025-02-12 09:39:24 +0100

    modified: test.py

--- test.py
+++ test.py
@@ -1,2 +1,15 @@
-print("Welkom bij Fonteyn Vakantieparken")
-print("Wij hebben 3 vakantieparken in Nederland")
+from datetime import datetime
+
+def decide():
+    current_time = datetime.now().time()
+    if current_time >= datetime.strptime("07:00", "%H:%M").time() and current_time < datetime.strptime("12:00", "%H:%M").time():
+        return "Goedemorgen"
+    elif current_time >= datetime.strptime("12:00", "%H:%M").time() and current_time < datetime.strptime("18:00", "%H:%M").time():
+        return "Goedemiddag"
+    elif current_time >= datetime.strptime("18:00", "%H:%M").time() and current_time < datetime.strptime("23:00", "%H:%M").time():
+        return "Goedenavond"
+    else:
+        return "Hallo"
+
+greeting = decide()
+print(greeting + "! Welkom bij Fonteyn Vakantieparken")
